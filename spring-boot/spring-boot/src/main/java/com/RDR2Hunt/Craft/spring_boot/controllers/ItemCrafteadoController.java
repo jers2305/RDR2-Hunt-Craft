9962457169283e8ad99b4a9192544c84a7075838
Javier <Javierr2305@gmail.com>
2025-06-07 19:50:08 +0200

    fixing

--- spring-boot/spring-boot/src/main/java/com/RDR2Hunt/Craft/spring_boot/controllers/ItemCrafteadoController.java
+++ spring-boot/spring-boot/src/main/java/com/RDR2Hunt/Craft/spring_boot/controllers/ItemCrafteadoController.java
@@ -26,6 +26,7 @@ public class ItemCrafteadoController {
     }
 
     @DeleteMapping("/delete/{id}")
+
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
